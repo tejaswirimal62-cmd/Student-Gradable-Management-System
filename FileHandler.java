938a2f5ab74b65f7ec21fb8db4cf3ebf2224ba2c
tejaswirimal62-cmd <tejaswirimal62@gmail.com>
2026-02-15 09:34:47 +0545

    FileHandler.java

--- FileHandler.java
+++ FileHandler.java
@@ -4,21 +4,25 @@ import java.util.List;
 public class FileHandler {
 
     public static void saveStudents(List<Student> students, String fileName) {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))) {
+        try (ObjectOutputStream out =
+                     new ObjectOutputStream(new FileOutputStream(fileName))) {
             out.writeObject(students);
-            System.out.println("Data saved to " + fileName);
+            System.out.println("Students saved to file.");
         } catch (IOException e) {
-            System.out.println("Error saving file: " + e.getMessage());
+            System.out.println("File save error: " + e.getMessage());
         }
     }
 
     @SuppressWarnings("unchecked")
     public static List<Student> loadStudents(String fileName) {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName))) {
+        try (ObjectInputStream in =
+                     new ObjectInputStream(new FileInputStream(fileName))) {
             return (List<Student>) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error loading file: " + e.getMessage());
+            System.out.println("File load error: " + e.getMessage());
             return null;
         }
     }
 }
+
+
