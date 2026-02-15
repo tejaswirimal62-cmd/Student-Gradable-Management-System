5120f0c57ab36187e12338c162d631a68aab5da7
tejaswirimal62-cmd <tejaswirimal62@gmail.com>
2026-02-15 09:36:25 +0545

    Storable.java

--- Storable.java
+++ Storable.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 public interface Storable {
-    void saveToFile(String fileName) throws IOException;
-    void loadFromFile(String fileName) throws IOException;
+    void saveData();
+    void loadData();
 }
+
